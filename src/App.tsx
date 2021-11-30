626578f8219a6f6d4ef3cf766cc64d1006e43ba3
HaqpaHR <pereverzev.viacheslav@gmail.com>
2021-11-30 19:21:17 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,153 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ChangeEvent,FormEvent} from "react";
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type State = {
+  cardNumber: string,
+  expired: string,
+  cvc: string,
+  typeOfCard: string,
 }
 
-export default App;
+export class App extends React.Component<{}, State> {
+  state = {
+    cardNumber: '',
+    expired: '',
+    cvc: '',
+    typeOfCard: '',
+  }
+
+  cardNumberHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      const reg = new RegExp('^[0-9 ]*$')
+      event.target.value = event.target.value.replace(/\s/g, '').replace(/(\d{4})/g, '$1 ').trim();
+
+      if (reg.test(event.target.value) && this.state.cardNumber.length <=18) {
+        this.setState({
+          cardNumber: event.target.value,
+        })
+      }
+
+      if (this.state.cardNumber[0] === '4' && this.state.cardNumber[1] === '9') {
+        this.setState(state => ({
+          ...state,
+          typeOfCard: 'https://logosklad.ru/photo/logos/290/1494134204.jpg#.YaY4hnOwdhg.link',
+        }))
+      }
+      if (this.state.cardNumber[0] === '5' && this.state.cardNumber[1] === '1') {
+        this.setState(state => ({
+          ...state,
+          typeOfCard: 'https://logosklad.ru/photo/logos/520/1468563734.jpg#.YaZb9vMkDIw.link',
+        }))
+      }
+
+  };
+
+    expiredHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+      function makeNumber(val = "") {
+        return val.replace(/\D+/g, "");
+      }
+
+      function expirationDate(value: string) {
+        const clearValue = makeNumber(value);
+        if(clearValue.length >= 3) {
+          return `${clearValue.slice(0, 2)}/${clearValue.slice(2, 4)}`
+        }
+        return clearValue;
+      }
+
+      this.setState({
+        expired: expirationDate(value),
+      })
+  };
+
+  cvcHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    const reg = new RegExp('^[0-9]*$');
+
+    if(value.match(reg) && this.state.cvc.length <= 2) {
+      this.setState({
+        cvc: value,
+      })
+    }
+  }
+
+  clearHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.setState({
+      cardNumber: '',
+      expired: '',
+      cvc: '',
+      typeOfCard: '',
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <p>Pay with your credit card via PayPal Website Payments Pro.</p>
+        <form
+          className="input"
+          onSubmit={this.clearHandler}
+        >
+          <label className="input__label" htmlFor='input-cardNumber'>
+            Card Number:
+          </label>
+            <input
+                className="input input__label--numCard"
+                type='text'
+                id="input-cardNumber"
+                name="cardNumber"
+                required
+                placeholder="•••• •••• •••• ••••"
+                value={this.state.cardNumber}
+                onChange={this.cardNumberHandler}
+            />
+
+          { this.state.typeOfCard &&(
+              <div className="image"><img src={`${this.state.typeOfCard}`} className="picture" /></div>
+          )}
+          <div className="input__bottom">
+            <div className="input__bottom-container">
+              <label className="input__label" htmlFor='input-expired'>
+                Expiry (MM/YY):
+              </label>
+                <input
+                    className="input input__label--expired"
+                    type='string'
+                    id="input-expired"
+                    name="expired"
+                    placeholder="MM/YY"
+                    required
+                    value={this.state.expired}
+                    onChange={this.expiredHandler}
+                />
+            </div>
+            <div className="input__bottom-container">
+              <label className="input__label" htmlFor='input-cvc'>
+                Card Code:
+              </label>
+                <input
+                    className="input input__label--cvc"
+                    type='string'
+                    id="input-cvc"
+                    name="cvc"
+                    required
+                    placeholder="CVC"
+                    value={this.state.cvc}
+                    onChange={this.cvcHandler}
+                />
+            </div>
+          </div>
+          <button
+              className="button"
+              type="submit"
+          >
+            Clear
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
